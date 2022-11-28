8e0dd27ac02b67cb420773c547e5e3b6fa746baf
Ktkenny127 <ktkenny@ucsd.edu>
2022-11-27 19:01:01 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -16,19 +16,7 @@ fi
 
 cd student-submission
 
-java -cp $CPATH  org.junit.runner.JUnitCore TestListExamples > failures.txt
-javac -cp $CPATH ListExamples.java 2>> errors.txt 
-
-
-if [[ $(grep -c "error: method filter" errors.txt) -ne 0 ]]
-then
-    echo "ERROR in Filter"
-fi
-
-if [[ $(grep -c "error: method merge" errors.txt) -ne 0 ]]
-then
-    echo "ERROR in Merge"
-fi
+java -cp $CPATH  org.junit.runner.JUnitCore TestListExamples > failures.txt 
 
 if [[ $(grep -c "testFilter(TestListExamples)" failures.txt) -ne 0 ]]
 then
