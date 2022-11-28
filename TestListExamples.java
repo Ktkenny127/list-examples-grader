86865d4c10738bb1430378e5d6c4b06ebef8f087
Ktkenny127 <ktkenny@ucsd.edu>
2022-11-27 18:44:55 -0800

    fish

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,10 +1,32 @@
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.*;
 
+class containsLetterA{
+  public boolean checkString(String s) {
+    return s.contains("a") || s.contains("a");
+  }
+}
 public class TestListExamples {
   @Test(timeout = 100)
   public void testTimeout() {
     while(true) {}
   }
-  // Write your grading tests here!
+  @Test(timeout = 1000)
+  public void testFilter() {
+    List<String> input1 = new ArrayList<String>(Arrays.asList("apple", "greed", "", "AAAAAaaa", "  a "));
+    assertEquals(new ArrayList<String>(Arrays.asList("apple", "AAAAAaaa", "  a ")), ListExamples.filter(input1, new containsLetterA()));
+  }
+
+@Test(timeout=1000)
+  public void testMerge() {
+    List<String> input1 = new ArrayList<String>(Arrays.asList("a", "c", "e"));
+    List<String> input2 = new ArrayList<String>(Arrays.asList("b", "d", "f"));
+    List<String> output = new ArrayList<String>(Arrays.asList("a", "b", "c", "d", "e", "f"));
+    assertEquals(output, ListExamples.merge(input1, input2));
+  }
 }
--- grade.sh
+++ grade.sh
@@ -1,9 +1,43 @@
-# Create your grading script here
+CPATH=".:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar"
+f="student-submission/ListExamples.java"
+
 
 rm -rf student-submission
 git clone $1 student-submission
-echo 'Finished cloning'
+cp TestListExamples.java student-submission/
 
+if [[ -f $f ]] && [[ -e $f ]]
+then
+    echo "File exists!"
+else
+    echo "File not found!"
+    exit 1
+fi
 
+cd student-submission
 
+java -cp $CPATH  org.junit.runner.JUnitCore TestListExamples > failures.txt
 
+if [[ $(grep -c "error: method filter" errors.txt) -ne 0 ]]
+then
+    echo "ERROR in Filter"
+fi
+
+if [[ $(grep -c "error: method merge" errors.txt) -ne 0 ]]
+then
+    echo "ERROR in Merge"
+fi
+
+if [[ $(grep -c "testFilter(TestListExamples)" failures.txt) -ne 0 ]]
+then
+    echo "FAILED testFilter"
+else
+    echo "PASSED testFilter"
+fi
+
+if [[ $(grep -c "testMerge(TestListExamples)" failures.txt) -ne 0 ]]
+then
+    echo "FAILED testMerge"
+else
+    echo "PASSED testMerge"
+fi
